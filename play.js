69cfa24d594022b357ef99549ac7a330718a30aa
ESDUNCAN <d.eric@rogers.com>
2022-05-11 22:41:33 +0000

    update

--- play.js
+++ play.js
@@ -1,22 +1,6 @@
 const { connectToSnake } = require("./client.js")
+const { setupInput } = require("./input.js")
 
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", (data) => {
-    handleUserInput(data)
-  });
-  return stdin;
-};
-
-const handleUserInput = function (data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
 
 
 connectToSnake()
